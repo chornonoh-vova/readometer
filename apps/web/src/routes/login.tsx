82bbdeb2c907de42b80d3fe1a67909a7dd5baa04
Volodymyr Chornonoh <chernonog.vova@gmail.com>
2026-09-22 11:39:01 +0300

    fix: typecheck error

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -6,12 +6,12 @@ import { z } from "zod";
 export const Route = createFileRoute("/login")({
   component: Login,
   validateSearch: z.object({
-    redirect: z.string().optional().prefault("/"),
+    redirect: z.string().optional(),
   }),
 });
 
 function Login() {
-  const { redirect } = Route.useSearch();
+  const { redirect = "/" } = Route.useSearch();
   return (
     <div className="bg-muted flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
